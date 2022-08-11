6d4ce58c1b0d706e303ce65a7abcb338fd434d1f
jefersonlopesl <jefersonlopesluis@duck.com>
2022-08-11 14:27:28 -0300

    tirei a head

---
+++ node_modules/react-terminal/dist/components/Terminal.d.ts
@@ -0,0 +1,33 @@
+import PropTypes from "prop-types";
+declare function Terminal(props: any): JSX.Element;
+declare namespace Terminal {
+    var propTypes: {
+        enableInput: PropTypes.Requireable<boolean>;
+        caret: PropTypes.Requireable<boolean>;
+        theme: PropTypes.Requireable<string>;
+        showControlBar: PropTypes.Requireable<boolean>;
+        showControlButtons: PropTypes.Requireable<boolean>;
+        controlButtonLabels: PropTypes.Requireable<string[]>;
+        prompt: PropTypes.Requireable<string>;
+        commands: PropTypes.Requireable<{
+            [x: string]: ((...args: any[]) => any) | PropTypes.ReactNodeLike;
+        }>;
+        welcomeMessage: PropTypes.Requireable<((...args: any[]) => any) | PropTypes.ReactNodeLike>;
+        errorMessage: PropTypes.Requireable<((...args: any[]) => any) | PropTypes.ReactNodeLike>;
+        defaultHandler: PropTypes.Requireable<(...args: any[]) => any>;
+    };
+    var defaultProps: {
+        enableInput: boolean;
+        caret: boolean;
+        theme: string;
+        showControlBar: boolean;
+        showControlButtons: boolean;
+        controlButtonLabels: string[];
+        prompt: string;
+        commands: {};
+        welcomeMessage: string;
+        errorMessage: string;
+        defaultHandler: any;
+    };
+}
+export default Terminal;
